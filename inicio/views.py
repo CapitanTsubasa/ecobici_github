1abcd3f6961b5678401727f8bc1d3d2d6dcd04b4
Nicolas <lbaez@buenosaires.gob.ar>
2025-07-18 11:39:24 -0300

    probando primeros html

--- inicio/views.py
+++ inicio/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {"mensaje": "Bienvenidos a la pagina de ecobici"}
+    return render(request, "index.html", context)
